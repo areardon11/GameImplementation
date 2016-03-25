d4fc65f2bcca50d6931f4f336aa5770bba1dce75
AndrewReardon <areardon11@hotmail.com>
2016-03-25 16:31:46 -0400

    bug fixed toot_and_otto_np

--- toot_and_otto_np.py
+++ toot_and_otto_np.py
@@ -8,18 +8,19 @@ LOSS, WIN, TIE, DRAW, UNDECIDED = "LOSS", "WIN", "TIE", "DRAW", "UNDECIDED"
 toot = np.array([1,2,2,1])
 otto = np.array([2,1,1,2])
 
+#feel free to change the height to reduce the problem size
 height = 4
 length = 6
 
 initial_position = np.zeros((height+1,length), dtype = np.int8)
 # top row is number of T for P1, O for P1, T for P2, O for P3, 
-# which player is toot, and the first player
-initial_position[0,] = [6, 6, 6, 6, 1, 1]
+# which players turn it is
+# assumes that player 1 always goes for toot
+initial_position[0,] = [6, 6, 6, 6, 1, 0]
 
 def print_board(board):
 	#prints the current board with helpful indices on the left and the bottom
-	for i in range(1,height+1):
-		print board[i]
+	print(board[1:,:])
 
 # def board_is_full(board):
 # 	bool_pieces = (board[1:height+1,]  == 0)
@@ -63,12 +64,10 @@ def word_test(board, x, y, word, dx, dy, char_pos_in_word):
 		return False
 	return word_test(board, x+dx, y+dy, word, dx, dy, char_pos_in_word+1)
 
-#Implementation of the neccessary functions for the solver
-
 #assumes that player1 goes for toot and player2 goes for otto
 
-#assumes that if the score is tied, continue playing no matter how many matches
-#takes in a state parameter which is a State object
+#assumes that if
+#takes in a state parameter which is a numpy array
 #returns a string of the options win, loss, tie, draw, unkwown
 
 def primitive(board):
@@ -77,12 +76,12 @@ def primitive(board):
 		return TIE
 	if score[0] >= 1:
 		print("toot wins")
-		if board[0,5] == 1:
+		if board[0,4] == 1:
 			return WIN
 		return LOSS
 	elif score[0] >= 1:
 		print("otto wins")
-		if board[0,5] == 1:
+		if board[0,4] == 1:
 			return LOSS
 		return WIN
 	if board_is_full(board):
@@ -91,7 +90,7 @@ def primitive(board):
 		return UNDECIDED
 
 #action is defined as a tuple with the letter, and a board location
-#example of an action: ("T", (2,3))
+#example of an action: (2, (2,3))
 
 #takes in the parameter state, a State object
 #returns a list of actions that are valid to be applied to the parameter state
@@ -101,15 +100,14 @@ def gen_moves(board):
 		hand = np.append(board[0,0],board[0,1])
 
 	possible_actions = []
-	for y in range(6):
-		x = 0
-		while x < 4 and board[4-x,y] != 0:
-			x += 1
-		if x < 4:
+	for y in range(length):
+		x = height
+		while x > 0 and not board[x,y] == 0:
+			x -= 1
+		if x > 0:
 			for i in range(2):
 				if hand[i]>0:
-					possible_actions.append((i+1, (4-x,y)))
-
+					possible_actions.append((i+1, (x,y)))
 	return possible_actions
 
 #returns the successor given by applying the parameter action to the parameter state
@@ -117,26 +115,26 @@ def gen_moves(board):
 #the parameter state is a State object
 #must pass in a valid state and a valid action for that state, does not check
 def do_move(board, action):
-	valid_moves = gen_moves(board)
-	if action not in valid_moves:
-		print 'INVALID MOVE'
-		return board
+	# valid_moves = gen_moves(board)
+	# if action not in valid_moves:
+	# 	print 'INVALID MOVE'
+	# 	return board
 
+	successor = np.copy(board)
 	piece, loc = action
 
-	board[loc] = piece
-	if board[0,4]==1 and piece == 1:
-		board[0,0] -= 1
-	elif board[0,4]==1 and piece == 2:
-		board[0,1] -= 1
-	elif board[0,4]==2 and piece == 1:
-		board[0,2] -= 1
+	successor[loc] = piece
+	if successor[0,4] == 1 and piece == 1:
+		successor[0,0] -= 1
+	elif successor[0,4] == 1 and piece == 2:
+		successor[0,1] -= 1
+	elif successor[0,4] == 2 and piece == 1:
+		successor[0,2] -= 1
 	else:
-		board[0,3] -= 1
+		successor[0,3] -= 1
 
-	board[0,4] = 1 + (board[0,4] % 2)
-
-	return board
+	successor[0,4] = 1 + (successor[0,4] % 2)
+	return successor
 
 
 #helpful prints for reference, understanding the code, and debugging
@@ -182,7 +180,21 @@ def example():
 	print 'primitive value:'
 	print primitive(board)
 
-	board = do_move(board, possible_actions[4])
+	board = do_move(board, possible_actions[5])
+	print 'this is the state after a move has been made'
+	print_board(board)
+	print 'hand1T=' + str(board[0,0])
+	print 'hand1O=' + str(board[0,1])
+	print 'hand2T=' + str(board[0,2])
+	print 'hand2O=' + str(board[0,3])
+	print 'firstPlayerTurn=' + str(board[0,4]==1)
+	possible_actions = gen_moves(board)
+	print 'New possible actions length:'
+	print len(possible_actions)
+	print 'primitive value:'
+	print primitive(board)
+
+	board = do_move(board, possible_actions[5])
 	print 'this is the state after a move has been made'
 	print_board(board)
 	print 'hand1T=' + str(board[0,0])
